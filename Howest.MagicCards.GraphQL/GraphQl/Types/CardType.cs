15f5ba18d9b10979647783bc0f248a3b7abd2457
Loesje <jarne.losschaert@student.howest.be>
2023-05-14 11:43:27 +0200

    added type card

---
+++ Howest.MagicCards.GraphQL/GraphQl/Types/CardType.cs
@@ -0,0 +1,36 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.Shared.DTO;
+
+namespace Howest.MagicCards.GraphQL.GraphQl.Types
+{
+    public class CardType : ObjectGraphType<CardDetailReadDTO>
+    {
+        public CardType()
+        {
+            Field(c => c.Id);
+            Field(c => c.Name);
+            Field(c => c.ManaCost);
+            Field(c => c.ConvertedManaCost);
+            Field(c => c.Type);
+            Field(c => c.RarityName);
+            Field(c => c.SetName);
+            Field(c => c.Text);
+            Field(c => c.Flavor);
+            Field(c => c.ArtistFullName);
+            Field(c => c.Number);
+            Field(c => c.Power);
+            Field(c => c.Toughness);
+            Field(c => c.Layout);
+            Field(c => c.MutiverseId);
+            Field(c => c.OriginalImageUrl);
+            Field(c => c.Image);
+            Field(c => c.OriginalText);
+            Field(c => c.OriginalType);
+            Field(c => c.MtgId);
+            Field(c => c.Variations);
+            Field(c => c.ColorCodes);
+            Field(c => c.TypeNames);
+        }
+    }
+}
--- Howest.MagicCards.GraphQL/Program.cs
+++ Howest.MagicCards.GraphQL/Program.cs
@@ -1,4 +1,12 @@
+using Howest.MagicCards.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager config = builder.Configuration;
+
+builder.Services.AddDbContext<MyCardsContext>
+    (options => options.UseSqlServer(config.GetConnectionString("CardsDb")));
+builder.Services.AddScoped<ICardRepository, SqlCardRepository>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
